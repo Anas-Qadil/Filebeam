d585861fb6e20f3085db071fcac39ffd031786ee
Anas Qadil <qadilanass10@gmail.com>
2023-01-12 11:15:48 +0100

    format file

--- upload.go
+++ upload.go
@@ -23,52 +23,52 @@ type Config struct {
 
 func uploadHandler(config Config) http.HandlerFunc {
     return func(w http.ResponseWriter, r *http.Request) {
-        err := r.ParseMultipartForm(config.MaxFileSize);
-        if err != nil {
-            http.Error(w, "File size exceeded the maximum allowed", http.StatusBadRequest);
-            return;
-        }
-        file, handler, err := r.FormFile("uploadfile");
-        if err != nil {
-            http.Error(w, "Error retrieving file", http.StatusBadRequest);
-            return;
-        }
-        defer file.Close();
-        filetype := strings.ToLower(filepath.Ext(handler.Filename));
-        if !contains(config.AllowedFileTypes, filetype) {
-            http.Error(w, "File type not allowed", http.StatusBadRequest);
-            return;
-        }
-        filename := handler.Filename;
-        if config.GenerateUniqueFilenames {
-            filename = strings.TrimSuffix(filename, filepath.Ext(filename)) + "-" + time.Now().Format("20060102150405") + filepath.Ext(filename);
-        }
-        f, err := os.OpenFile(filepath.Join(config.UploadDir, filename), os.O_WRONLY|os.O_CREATE, 0666);
-        if err != nil {
-            http.Error(w, "Error creating file", http.StatusInternalServerError);
-            return;
-        }
-        defer f.Close();
-        io.Copy(f, file);
-        w.Write([]byte("File uploaded successfully."));
+			err := r.ParseMultipartForm(config.MaxFileSize);
+			if err != nil {
+				http.Error(w, "File size exceeded the maximum allowed", http.StatusBadRequest);
+				return;
+			}
+			file, handler, err := r.FormFile("uploadfile");
+			if err != nil {
+				http.Error(w, "Error retrieving file", http.StatusBadRequest);
+				return;
+			}
+			defer file.Close();
+			filetype := strings.ToLower(filepath.Ext(handler.Filename));
+			if !contains(config.AllowedFileTypes, filetype) {
+					http.Error(w, "File type not allowed", http.StatusBadRequest);
+					return;
+			}
+			filename := handler.Filename;
+			if config.GenerateUniqueFilenames {
+					filename = strings.TrimSuffix(filename, filepath.Ext(filename)) + "-" + time.Now().Format("20060102150405") + filepath.Ext(filename);
+			}
+			f, err := os.OpenFile(filepath.Join(config.UploadDir, filename), os.O_WRONLY|os.O_CREATE, 0666);
+			if err != nil {
+					http.Error(w, "Error creating file", http.StatusInternalServerError);
+					return;
+			}
+			defer f.Close();
+			io.Copy(f, file);
+			w.Write([]byte("File uploaded successfully."));
     }
 }
 
 func contains(s []string, e string) bool {
     for _, a := range s {
-        if a == e {
-            return (true);
-        }
+			if a == e {
+				return (true);
+			}
     }
     return (false);
 }
 
 func main() {
     config := Config{
-        MaxFileSize:           32 << 20, // 32MB
-        AllowedFileTypes:      []string{".jpg", ".jpeg", ".png", ".gif"},
-        UploadDir:             "uploads/",
-        GenerateUniqueFilenames: true,
+			MaxFileSize:           		32 << 20, // 32MB
+			AllowedFileTypes:					[]string{".jpg", ".jpeg", ".png", ".gif"},
+			UploadDir:								"uploads/",
+			GenerateUniqueFilenames:	true,
     }
 		// Create the upload directory if it doesn't exist
 		if _, err := os.Stat(config.UploadDir); os.IsNotExist(err) {
